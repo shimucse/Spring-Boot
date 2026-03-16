711176bfbd3d82f13e69c3519e23893295f403ae
shimucse <abisheema@gmail.com>
2026-03-16 11:01:17 +0600

    Section-4:REST CRUD APIs

---
+++ section4-rest-crud-api/src/main/java/com/shima/section4_rest_crud_api/dao/EmployeeDAOJpaImp.java
@@ -0,0 +1,47 @@
+package com.shima.section4_rest_crud_api.dao;
+
+import com.shima.section4_rest_crud_api.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public class EmployeeDAOJpaImp implements EmployeeDAO{
+   //define field for entityManager
+    private EntityManager entityManager;
+
+    //set up constructor injection
+    //the EntityManager is automatically created by Spring boots
+
+    @Autowired
+    public EmployeeDAOJpaImp(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> query = entityManager.createQuery("from Employee", Employee.class);
+        List<Employee> employees = query.getResultList();
+        return employees;
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        Employee dbEmployee = entityManager.merge(theEmployee);
+        return dbEmployee;
+    }
+
+    @Override
+    public void delete(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        entityManager.remove(theEmployee);
+    }
+}
---
+++ section4-rest-crud-api/src/main/java/com/shima/section4_rest_crud_api/rest/EmployeeRestController.java
@@ -0,0 +1,67 @@
+package com.shima.section4_rest_crud_api.rest;
+
+import com.shima.section4_rest_crud_api.entity.Employee;
+import com.shima.section4_rest_crud_api.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeService employeeService;
+    // You only need JsonMapper if you implement PATCH (partial update).
+    private JsonMapper josonMapper;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService, JsonMapper theJsonMapper){
+        employeeService = theEmployeeService;
+        josonMapper = theJsonMapper;
+    }
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public  Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee ==null){
+            throw new RuntimeException("Employee id not found -"+employeeId);
+        }
+        return theEmployee;
+    }
+    @PostMapping("/employees")
+    //@RequestBody: “Take the body of the HTTP request and map it to this Java object.
+    public  Employee addEmployee(@RequestBody  Employee theEmployee){
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+    @PatchMapping("/employees/{employeeId}")
+    public  Employee patchEmployee(@PathVariable int employeeId,@RequestBody Map<String, Object> patchPayload){
+        Employee tempEmployee = employeeService.findById(employeeId);
+        if(tempEmployee==null){
+            throw new RuntimeException("Employee id not found -"+employeeId);
+        }
+        if(patchPayload.containsKey("id")){
+            throw new RuntimeException("Employee id not allowed in request body -"+employeeId);
+        }
+        Employee patchedEmployee = josonMapper.updateValue(tempEmployee, patchPayload);
+        Employee dbEmployee = employeeService.save(patchedEmployee);
+        return dbEmployee;
+    }
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+         Employee theEmployee = employeeService.findById(employeeId);
+         if(theEmployee==null){
+             throw new RuntimeException("Employee if not found -"+employeeId);
+         }
+         employeeService.delete(employeeId);
+         return "Deleted employee id -"+employeeId;
+    }
+
+}
